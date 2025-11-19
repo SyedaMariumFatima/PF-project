5cc4bafcaec99a2bc0701fe5c62ad850563f4898
Rabisa <rabisa55511@gmail.com>
2025-11-19 15:46:15 +0500

    updating code

--- code.c
+++ code.c
@@ -2,92 +2,104 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
-#include <conio.h>
 
 #define MAX_WORDS 1000
 
+int game(char* answer, char* guess);
 int processGuess(char *answer, char *guess);
-int main()
-{
+int total_guesses = 0;
 
-    char **wordsList = calloc(MAX_WORDS, sizeof(char *)); // array of strings
+int main() {
+    char **wordsList = calloc(MAX_WORDS, sizeof(char *));
     int wordCount = 0;
-    char *fiveLetterWord = malloc(6 * sizeof(char));
-    FILE *words = fopen("words.txt", "r"); // accessing word file
+    char buffer[6];
+    FILE *words = fopen("words.txt", "r");
 
-    // loading words into the game
-    while (fscanf(words, "%s", fiveLetterWord) != EOF && wordCount < MAX_WORDS)
-    {
-        wordsList[wordCount] = fiveLetterWord;
+    if (words == NULL) {
+        printf("Failed to open words.txt\n");
+        return 1;
+    }
+
+    while (fscanf(words, "%5s", buffer) != EOF && wordCount < MAX_WORDS) {
+        wordsList[wordCount] = strdup(buffer);
         wordCount++;
-        fiveLetterWord = malloc(6 * sizeof(char));
     }
     fclose(words);
+    srand(time(NULL));
+    char *guess = malloc(6 * sizeof(char));
+    int number_of_tries = 0;
+    char response[4];
 
-    // starting the game
-    srand(time(NULL)); // randomly picking a word
-    char *answer = wordsList[rand() % wordCount];
+    printf("Welcome to Wordle\n");
+    printf("You'll have 6 tries to guess a word\n");
+    printf("Correct letters in correct spots: \033[92mgreen\033[0m\n");
+    printf("Correct letters in wrong spots: \033[93myellow\033[0m\n");
+    printf("Incorrect letters: \033[91mred\033[0m\n");
+    printf("Let's start the game!\n");
 
-    int guesses = 0; // total number of guesses
-    bool correctGuess = 0;
-    char *guess = malloc(6 * sizeof(char)); // storing that word
+    do {
+        char *answer = wordsList[rand() % wordCount];
+        int guesses_used = game(answer, guess);
+        total_guesses += guesses_used;
 
-    while (guesses < 6)
-    {
-        printf("\033[94m\nEnter a 5-letter word and press enter: ");
-        scanf("%s", guess);
-        guesses++;
-
-        if (processGuess(answer, guess) == 5)
-        {
-            printf("\033[92myou win!");
+        if (guesses_used <= 6) {
+            printf("\033[92m\nYou Won!\033[0m\n");
+            printf("Do you wish to play again? Enter Yes or No: ");
+            scanf("%3s", response);
+        } else {
+            printf("\033[93mYou lost!\033[0m\n");
             break;
         }
+        number_of_tries++;
+    } while ((strcasecmp(response, "YES") == 0) && number_of_tries < 10);
+
+    if (number_of_tries > 0) {
+        float avg_steps = (float)total_guesses / number_of_tries;
+        printf("You played Wordle %d times and guessed the word(s) in an average of %.2f steps\n", number_of_tries, avg_steps);
     }
 
-    // freeing the memory used
-    for (int i = 0; i < wordCount; i++)
-    {
-        free(wordsList[i]); // freeing individual words memory
+    for (int i = 0; i < wordCount; i++) {
+        free(wordsList[i]);
     }
-    free(wordsList); // freeing the entire list
-    free(fiveLetterWord);
+    free(wordsList);
     free(guess);
 
     return 0;
 }
-
-int processGuess(char *answer, char *guess)
-{
-    int corr = 0;
-    // for correct letter in correct place
-    for (int i = 0; i < 5; i++)
-    {
-
-        if (guess[i] == answer[i])
-        {
-            printf("\033[92m%c", guess[i]);
-            corr++;
-        }
-        else
-        {
-            int count = 0;
-            for (int j = 0; j < 5; j++)
-            {
-                if (guess[i] == answer[j] && i != j)
-                {
-                    count++;
+int processGuess(char *answer, char *guess) {
+    int correct = 0;
+    for (int i = 0; i < 5; i++) {
+        if (guess[i] == answer[i]) {
+            printf("\033[92m%c\033[0m", guess[i]);
+            correct++;
+        } else {
+            int found = 0;
+            for (int j = 0; j < 5; j++) {
+                if (guess[i] == answer[j] && i != j) {
+                    found = 1;
+                    break;
                 }
             }
-            if (count == 0)
-            {
-                printf("\033[91m%c", guess[i]);
-            }
+            if (found)
+                printf("\033[93m%c\033[0m", guess[i]);
             else
-
-                printf("\033[93m%c", guess[i]);
+                printf("\033[91m%c\033[0m", guess[i]);
         }
     }
-    return corr;
+    printf("\n");
+    return correct;
 }
+
+int game(char* answer, char* guess) {
+    int guesses = 0;
+    while (guesses < 6) {
+        printf("\033[94mEnter a 5-letter word: \033[0m");
+        scanf("%5s", guess);
+        guesses++;
+        if (processGuess(answer, guess) == 5) {
+            return guesses;
+        }
+    }
+    return 7; // Indicates loss
+}
+
